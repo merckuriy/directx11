58217ecd0fd838853f437790bb781f2b60980927
Viktor <30699669+merckuriy@users.noreply.github.com>
2021-07-20 17:16:20 +0300

    Add files via upload

---
+++ https:/github.com/merckuriy/directx11/tree/main/CubeAround/main.cpp
@@ -0,0 +1,20 @@
+#include "MyRender.h"
+
+int main()
+{
+	Framework framework;
+	MyRender *render = new MyRender();
+
+	FrameworkDesc desc;
+	desc.wnd.width = 640;
+	desc.wnd.height = 480;
+	desc.render = render;
+
+	framework.Init(desc);
+
+	framework.Run();
+
+	framework.Close();
+
+	return 0;
+}
